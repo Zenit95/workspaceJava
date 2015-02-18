a7e75e604e01d3ca835fb18f4fd7cce4d948e153
Zenit95 <javiermartineztejero@gmail.com>
2015-02-18 18:14:00 +0100

    me aburro

---
+++ PrimerosProyectosJava/src/sustituyeCaracteresRecu.java
@@ -0,0 +1,22 @@
+
+public class sustituyeCaracteresRecu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String palabra="supercalifragilisticoespialidoso";
+		String nuevaPalabra = sustituye(palabra);
+		System.out.println(palabra);
+		System.out.println(nuevaPalabra);
+	}
+
+	private static String sustituye(String pal){
+		String aux="";
+		aux= pal.charAt(0)+"";
+		for(int i=1; i<pal.length()-1;i++){
+			aux += "x";//aux = aux  + "x";
+		}
+		aux += pal.charAt(pal.length()-1);
+		
+		return aux;
+	}
+}
